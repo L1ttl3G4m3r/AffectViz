a784e2f903678c95ac510f37ef940a4740fa16c8
L1ttl3G4m3r <kaylee.hoek@hva.nl>
2025-12-15 12:02:24 +0100

    first setup with components and rescalable dashboard

---
+++ AffectViz/src/lib/dashboard/DashboardGrid.svelte
@@ -0,0 +1,32 @@
+<script>
+    import { onMount } from 'svelte';
+
+    import ChartWidget from './widgets/ChartWidget.svelte';
+    import StatsWidget from './widgets/StatsWidget.svelte';
+
+    let grid;
+
+    onMount(async () => {
+    const { GridStack } = await import('gridstack');
+
+    grid = GridStack.init({
+      float: true,
+      cellHeight: 80,
+      resizable: { handles: 'se' }
+    });
+  });
+</script>
+
+<div class="grid-stack">
+    <div class="grid-stack-item" gs-w="4" gs-h="2">
+        <div class="grid-stack-item-content">
+            <ChartWidget title="Sample" />
+        </div>
+    </div>
+
+    <div class="grid-stack-item" gs-w="3" gs-h="2">
+        <div class="grid-stack-item-content">
+            <StatsWidget />
+        </div>
+    </div>
+</div>
--- AffectViz/src/routes/+page.svelte
+++ AffectViz/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import DashboardGrid from "$lib/dashboard/DashboardGrid.svelte";
+</script>
+
+<h1>My Dashboard</h1>
+
+<DashboardGrid />
